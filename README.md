7227b4c91f61012905f212c6e317e0fb27dbcb03
Micah Woods <mwoods79@users.noreply.github.com>
2024-03-31 13:47:14 -0400

    Release version 0.0.2

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ by adding `envious` to your list of dependencies in `mix.exs`:
 ```elixir
 def deps do
   [
-    {:envious, "~> 0.1.0"}
+    {:envious, "~> 0.0.2"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Envious.MixProject do
   def project do
     [
       app: :envious,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
