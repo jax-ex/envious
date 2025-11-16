d009d2877b6760f537d21ecce4ef9b873e24d8d3
Micah Woods <mwoods79@users.noreply.github.com>
2025-11-15 21:33:55 -0500

    Remove unused config folder

--- config/runtime.exs
+++
@@ -1,6 +0,0 @@
-# Enum.each([".env", ".#{config_env()}.env"], fn file ->
-#   with {:ok, file} <- File.read(".env"),
-#        {:ok, envs} <- Dotenvy.Parser.parse(file) do
-#     Sytem.put_env(envs)
-#   end
-# end)
