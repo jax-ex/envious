206b6e0a4c56baf536234846322f0a09017132e5
Micah Woods <mwoods79@users.noreply.github.com>
2025-12-06 21:21:41 -0500

    Release version 1.4.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Envious.MixProject do
   def project do
     [
       app: :envious,
-      version: "1.3.0",
+      version: "1.4.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
